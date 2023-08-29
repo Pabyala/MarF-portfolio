2313375b95ea3f7f977a1c7a738e8e37d2f34d89
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-08-29 13:01:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { Pages } from "./Pages/Pages";
+import { Footer } from "./Pages/Footer";
+import { Navbar } from './Pages/Navbar';
+import { BackToTop } from './Components/BackToTop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar/>
+      <Pages/>
+      <BackToTop/>
+      <Footer/>
     </div>
   );
 }
